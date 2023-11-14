d77811ffca029cc9f2e1405524e9d5e7e9f5f439
mjconl6291 <mjconl6291@gmail.com>
2023-11-14 14:53:16 -0600

    testing trigger

--- src/app.py
+++ src/app.py
@@ -1,2 +1,7 @@
+import json
+
 def handler(event, context):
-    ...
+    body = event['Records'][0]['Sns']['Message']
+    body = json.loads(body)
+    operation = body['operation']
+    print(operation)
