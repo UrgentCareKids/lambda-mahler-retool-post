4eca35b4a6b2ca0fc96a8d788da9034de92144ad
mjconl6291 <mjconl6291@gmail.com>
2023-12-14 14:27:58 -0600

    troubleshooting

--- src/listener.py
+++ src/listener.py
@@ -21,6 +21,7 @@ def get_db_params():
     return db_params
 
 def run_app_py(operation, queue_id):
+    print('V1')
     subprocess.run(["python3", "/home/ubuntu/repos/lambda-mahler-retool-post/src/app_local.py", operation, str(queue_id)])
 
 def main():
